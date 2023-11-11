9f07f257e15fda58b9edb52cf327b05402218d25
Daniel <hamm.daniel@gmail.com>
2023-11-11 10:48:19 +0100

    Adding real implementation of fen_to_tensor to the dataloader.

--- src/PositionToEvaluationDataset.py
+++ src/PositionToEvaluationDataset.py
@@ -2,18 +2,20 @@ import pandas as pd
 import torch
 from torch.utils.data import Dataset
 
+from src.util.utilities import fen_to_tensor
+
 
 #########################################################################
 # This class is a dataset which reads .csv files with
 # (FEN) positions and an evaluation.
 #########################################################################
+def to_tensor(fen_position):
+    return fen_to_tensor(fen_position)
+
+
 class PositionToEvaluationDataset(Dataset):
     def __init__(self, csv_file):
         self.data = pd.read_csv(csv_file)
-
-    def to_tensor(self, string):
-        # FIXME: transform FEN to tensor
-        pass
 
     def __len__(self):
         return len(self.data)
@@ -21,6 +23,6 @@ class PositionToEvaluationDataset(Dataset):
     def __getitem__(self, idx):
         x_fen = self.data.iloc[idx, 0]  # FEN position
 
-        x = self.to_tensor(x_fen)  # Convert string to tensor
+        x = to_tensor(x_fen)  # Convert string to tensor
         y = torch.tensor(self.data.iloc[idx, 1], dtype=torch.float32)  # position evaluation
         return x, y
