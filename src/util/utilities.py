e2566b3419ee97684e1e5bfc81530d6c0ab99bb7
Daniel <hamm.daniel@gmail.com>
2023-11-13 19:53:47 +0100

    Added new method fen_to_tensor_one_board, which returns the same information as fen_to_tensor, but handles the dimensions a bit different (it creates just one board and for each field a 12 dimensional vector to represent the piece sitting on that field).

--- src/util/utilities.py
+++ src/util/utilities.py
@@ -1,5 +1,4 @@
 import chess
-import numpy as np
 import torch
 
 CLASS_WIN = 1
@@ -45,7 +44,7 @@ def is_stalemate(fen) -> bool:
 
 def fen_to_tensor(fen):
     """
-    Converts FEN into a tenor of 384 representing the board.
+    Converts FEN into a tensor of 12x64 dimensions representing the board.
 
     Parameters
     ----------
@@ -54,7 +53,7 @@ def fen_to_tensor(fen):
     Returns
     -------
     Array size  12 (Figures 6xWHITE, 6xBLACK) 8x8 (board). First 12 values represent the figures.
-    For each color and type there is an array of 64 fields.
+    For each color and type, there is an array of 64 fields.
     Values are 1 for turn -1 for wait.
 PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING
     Arrays:
@@ -74,22 +73,64 @@ PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING
     board = chess.Board()
     board.set_fen(fen)
 
-    result = np.zeros((12, 64))
+    result = torch.zeros((12, 64))
     for sq in chess.SQUARES:
         piece_type = board.piece_type_at(sq)
-        if piece_type != 0: # Not no color
-            if board.color_at(sq) == chess.WHITE: # white color on layer 1-6
+        if piece_type != 0:  # Not no color
+            if board.color_at(sq) == chess.WHITE:  # white color on layer 1-6
                 piece_layer = piece_type
-            else: # black color on layer 6-12
+            else:  # black color on layer 6-12
                 piece_layer = int(piece_type or 0) + 6
             piece_layer = piece_layer - 1
-            result[piece_layer, sq] =  (-1 if board.color_at(sq) == chess.WHITE else 1) * (-1 if board.turn == chess.WHITE else 1)
+            result[piece_layer, sq] = (-1 if board.color_at(sq) == chess.WHITE else 1) * \
+                                      (-1 if board.turn == chess.WHITE else 1)
     return result
+
+
+def fen_to_tensor_one_board(fen):
+    """
+    Converts FEN into a tensor of 64x12 dimensions representing the board.
+
+    While fen_to_tensor creates 12 different board representations (one for each piece type), this
+    resulting tensor creates just one board representation. For each field, the piece is encoded, so we have
+    64 fields and on each field 12 dimensions which encode the piece.
+    """
+
+    # Define the piece types
+    pieces = 'pnbrqkPNBRQK'
+    piece_to_index = {piece: index for index, piece in enumerate(pieces)}
+
+    # Initialize an empty board
+    board = torch.zeros(64, 12)
+
+    # Split the FEN string to get the board layout and current player
+    position, player = fen.split(' ')[0:2]
+
+    # Replace numbers with the corresponding number of empty squares
+    for i in range(1, 9):
+        position = position.replace(str(i), '.' * i)
+
+    # Replace slashes with empty spaces
+    position = position.replace('/', '')
+
+    # Fill the board tensor
+    for i, piece in enumerate(position):
+        if piece in piece_to_index:
+            # Calculate the piece's binary value (1 for current player, -1 for opponent)
+            if (player == 'w' and piece.isupper()) or (player == 'b' and not piece.isupper()):
+                value = 1
+            else:
+                value = -1
+            board[i, piece_to_index[piece]] = value
+
+    # Reshape the tensor to the desired shape (768,)
+    return board.view(-1)
 
 
 def main():
     tensor = fen_to_tensor("r1bqnrk1/2p2pbp/p1n1p1p1/1p1pP2P/3P1P2/3BBN2/PPP1N1P1/R2QK2R w")
     print(tensor)
+
 
 if __name__ == "__main__":
     main()
