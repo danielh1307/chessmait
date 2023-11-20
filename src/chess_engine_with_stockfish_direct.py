5b859dd779f3bd239cc139095cd6400f26204a40
anschello <angelo.casanova@bluewin.ch>
2023-11-20 14:46:19 +0100

    our models play against the stockfish chess engine

--- src/chess_engine_with_stockfish_direct.py
+++ src/chess_engine_with_stockfish_direct.py
@@ -1,6 +1,9 @@
 import os
 import src.board_status as bs
 from stockfish import Stockfish
+import src.chess_engine as ce
+import chess
+from timeit import default_timer as timer
 
 PATH_TO_CHESS_ENGINE = os.path.join("stockfish", "stockfish-windows-x86-64-avx2.exe")
 
@@ -15,47 +18,69 @@ PATH_TO_CHESS_ENGINE = os.path.join("stockfish", "stockfish-windows-x86-64-avx2.
 # eight         1000   22      +20               3000
 
 def run():
+    board = chess.Board()
     board_status = bs.BoardStatus()
 
-    stop = False
     do_not_stop = False
-    round = 0
-    max_rounds = 10
+    max_rounds = 100
+
+    start = timer()
+
+    model_white = ce.get_model_white()
 
     stockfish = Stockfish(path=PATH_TO_CHESS_ENGINE)
-    #stockfish.set_elo_rating(500)
-    stockfish.set_skill_level(20)
+    #stockfish.set_elo_rating(1000)
+    stockfish.set_skill_level(0)
     #stockfish.set_depth()
 
-    while not stop:
-        round += 1
-        is_white = (round % 2) == 1
-        board_status.cache_stockfish(stockfish.get_board_visual())
-        print(f"--------------- Round: {round:02d} {'white' if is_white else 'black'}", end='')
-        wdl = stockfish.get_wdl_stats()
-        if wdl == None:
-            print("")
-        else:
-            print(f" --- win: {wdl[0]} --- draw: {wdl[1]} --- loss: {wdl[2]}")
-        best_move = stockfish.get_best_move()
-        if best_move == None:
-            message = "game over: winner is " + ("black" if is_white else "white")
-            print(message)
-            break
-        stockfish.make_moves_from_current_position([best_move])
-        board_status.print_stockfish(stockfish.get_board_visual())
-        if not do_not_stop:
-            console_input = input("continue/stop/automatic/max-rounds ('Enter'/s/a/#)? ")
-            if console_input == 's':
-                stop = True
-            elif console_input == 'a':
-                do_not_stop = True
-            elif console_input != "" and console_input.isdigit():
-                max_rounds = int(console_input)
-                do_not_stop = True
-        if round > max_rounds:
-            stop = True
+    wins = {True: 0, False: 0}
+    for i in range(100):
 
+        board.reset_board()
+        stop = False
+        round = 0
+        do_not_stop = False
+
+        while not stop:
+            round += 1
+            is_white = (round % 2) == 1
+            board_status.cache(board)
+            #print(f"--------------- Round: {round:02d} {'white' if is_white else 'black'}")
+            if is_white:
+                ce.play(board, model_white, is_white)
+            else:
+                stockfish.set_fen_position(board.fen())
+                best_move = stockfish.get_best_move()
+                if best_move == None:
+                    message = "game over: winner is " + ("black" if is_white else "white")
+                    print(message)
+                    break
+                board.push_uci(best_move)
+            #board_status.print(board)
+            '''
+            if not do_not_stop:
+                console_input = input("continue/stop/automatic/max-rounds ('Enter'/s/a/#)? ")
+                if console_input == 's':
+                    stop = True
+                elif console_input == 'a':
+                    do_not_stop = True
+                elif console_input != "" and console_input.isdigit():
+                    max_rounds = int(console_input)
+                    do_not_stop = True
+            '''
+            if board.is_game_over() or round > max_rounds:
+                #print(f"--------------- Round: {round:02d} {'white' if is_white else 'black'}")
+                stop = True
+
+        #board_status.print(board)
+        #print("--------------------------------------------")
+        #print(board_status.reason_why_the_game_is_over(board, round > max_rounds))
+        #print("--------------------------------------------")
+        wins[is_white] += 1
+        print(f"wins: white = {wins[True]} - black = {wins[False]}")
+
+    end = timer()
+    print(f"time: {(end - start):0.1f} sec")
 
 if __name__ == "__main__":
     run()
