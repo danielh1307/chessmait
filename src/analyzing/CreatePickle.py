b15b422888cee87d9a263ee56218ecc959664858
Daniel <hamm.daniel@gmail.com>
2023-11-21 14:48:06 +0100

    Removed unnecessary code and added log output.

--- src/analyzing/CreatePickle.py
+++ src/analyzing/CreatePickle.py
@@ -5,9 +5,8 @@ import time
 import pandas as pd
 
 from src.lib.utilities import fen_to_tensor_one_board
-
-PATH_TO_DATAFILE = os.path.join("data", "preprocessed")
-PATH_TO_PICKLEFILE = os.path.join("data", "pickle")
+PATH_TO_DATAFILE = os.path.join("data", "preprocessed-classification")
+PATH_TO_PICKLEFILE = os.path.join("data", "pickle-classification-fen-to-tensor-one-board")
 
 
 def to_tensor(fen_position):
@@ -15,14 +14,10 @@ def to_tensor(fen_position):
 
 
 matching_files = [file for file in os.listdir(PATH_TO_DATAFILE) if
-                  fnmatch.fnmatch(file, "kaggle_preprocessed.csv")]
+                  fnmatch.fnmatch(file, "kaggle_preprocessed_1000.csv")]
 file_names = [os.path.basename(file) for file in matching_files]
 for file_name in file_names:
-    df = pd.read_csv(f"data/preprocessed/{file_name}")
-
-    if df["Evaluation"].dtype == 'object':
-        df = df[~df['Evaluation'].str.startswith('#')]
-        df["Evaluation"] = df["Evaluation"].astype(int)
+    df = pd.read_csv(os.path.join(PATH_TO_DATAFILE, file_name))
 
     print("Converting FEN to tensor ...")
     start_time = time.time()
@@ -31,3 +26,4 @@ for file_name in file_names:
     print(f"Done, it took me {end_time - start_time}s to do so ...")
 
     df.to_pickle(f"{PATH_TO_PICKLEFILE}/{file_name[0:-4]}.pkl")
+print("Finished")
