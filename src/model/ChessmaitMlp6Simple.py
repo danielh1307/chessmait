a901487266ddd984258f1b7f5be772cbcf407dfb
fzimmerli <frank@zimmerli.li>
2023-11-24 17:13:57 +0100

    Description adapted

--- src/model/ChessmaitMlp6Simple.py
+++ src/model/ChessmaitMlp6Simple.py
@@ -1,22 +1,17 @@
 import torch.nn as nn
 
 
-# Network is based on the MLP bitmap architecture described
-# in https://www.ai.rug.nl/~mwiering/GROUP/ARTICLES/ICPRAM_CHESS_DNN_2018.pdf
-# This model has more neurons than ChessmaitMlp1, but it is more similar to it
-# than ChessmaitMlp4. It also uses ReLU and a dropout instead of Batch Normalization.
-# The batch normalization does not work well with Adam, see — for example, this run
-# here: https://wandb.ai/chessmait/chessmait/runs/bmdm0m4t?workspace=user-hamm-daniel
+# Network base on ChessmaitMlp5 but with the adaption to the tensor fen_to_tensor_simple.
 class ChessmaitMlp6Simple(nn.Module):
     def __init__(self):
         """
         Initializes a multi-layer perceptron (MLP) model for a regression task.
 
         This model consists of four sequential layers:
-        - Layer 1: Input size 768, output size 2048, ReLU activation, and 20% dropout.
-        - Layer 2: Input size 2048, output size 2048, ReLU activation, and 20% dropout.
-        - Layer 3: Input size 2048, output size 2048, ReLU activation, and 20% dropout.
-        - Output Layer: Input size 2048, output size 1.
+        - Layer 1: Input size 769, output size 1538, ReLU activation, and 20% dropout.
+        - Layer 2: Input size 1538, output size 1538, ReLU activation, and 20% dropout.
+        - Layer 3: Input size 1538, output size 1538, ReLU activation, and 20% dropout.
+        - Output Layer: Input size 1538, output size 1.
 
         The forward pass applies these layers sequentially to the input tensor.
 
