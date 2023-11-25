72b935be909abbafe9e4fa0c0b730b7b6c84a1d9
anschello <angelo.casanova@bluewin.ch>
2023-11-25 11:12:19 +0100

    - colors of winner - 9x9 cache instead of 9x3x3

--- src/rl_tic_tac_toe.py
+++ src/rl_tic_tac_toe.py
@@ -3,13 +3,17 @@ import pettingzoo.utils as utils
 import numpy as np
 
 
+COLOR_WIN = '\033[1;91m'
+COLOR_DEFAULT = '\033[0m'
+
+
 def run(env):
 
     observation, reward, termination, truncation, info = env.last()
 
     total_reward = 0.0
 
-    cache = np.zeros((9,3,3))
+    cache = np.zeros((9,9))
 
     round = 0
 
@@ -26,48 +30,61 @@ def run(env):
 
         cache_board(cache, round, env.board)
         if termination:
-            if check_winner(cache[round]):
-                print(f"won by: {agent}")
+            winner, indexes = check_winner(cache[round])
+            if winner:
+                print(f"{COLOR_WIN}won by: {agent}{COLOR_DEFAULT}")
             else:
                 print("draw")
 
         round += 1
 
-    print_cache(cache, round)
+    print_cache(cache, round, indexes)
 
 
-def print_cache(cache, round):
+def print_cache(cache, round, indexes):
     for i in range(round):
         print(f"{i+1}-- | ", end='')
     print()
-    for i in range(3):
-        for j in range(round):
-            print_line(cache[j][i])
+    for row in range(3):
+        for r in range(round):
+            if r == round-1:
+                print_winner_line(cache[r], row, indexes)
+            else:
+                print_line(cache[r], row)
         print()
 
 
-def print_line(line):
-    print(f"{line[0]:.0f}{line[1]:.0f}{line[2]:.0f} | ", end='')
+def print_line(line, row):
+    for i in range(3):
+        print(f"{line[row*3+i]:.0f}", end='')
+    print(" | ", end='')
+
+
+def print_winner_line(line, row, indexes):
+    for i in range(3):
+        if (row*3+i) in indexes and indexes != (0,0,0):
+            print(f"{COLOR_WIN}{line[row*3+i]:.0f}{COLOR_DEFAULT}", end='')
+        else:
+            print(f"{line[row*3+i]:.0f}", end='')
+    print(" | ", end='')
 
 
 def cache_board(cache, round, board):
-    board = np.array(board.squares)
-    cache[round][0] = board[0:3]
-    cache[round][1] = board[3:6]
-    cache[round][2] = board[6:9]
+    cache[round] = np.array(board.squares)
 
 
 def check_winner(board):
+    x = np.resize(board, (3,3))
     for i in range(3):
-        if board[i][0] == board[i][1] == board[i][2]: # horizontal
-            return True
-        if board[0][i] == board[1][i] == board[2][i]: # vertical
-            return True
-    if board[0][0] == board[1][1] == board[2][2]:  # diagonal left/upper to right/lower
-        return True
-    if board[0][2] == board[1][1] == board[2][0]:  # diagonal left/lower to right/upper
-        return True
-    return False
+        if board[i*3] == board[i*3+1] == board[i*3+2] > 0: # horizontal
+            return True, (i*3, i*3+1, i*3+2)
+        if board[i] == board[i+3] == board[i+6]: # vertical
+            return True, (i, i+3, i+6)
+    if board[0] == board[4] == board[8]:  # diagonal left/upper to right/lower
+        return True, (0, 4, 8)
+    if board[6] == board[4] == board[2]:  # diagonal left/lower to right/upper
+        return True, (6, 4, 2)
+    return False, (0, 0, 0)
 
 if __name__ == "__main__":
     print("RL start ---")
