09107a5a65cbb681a5743cd477edcff9c8e932aa
Daniel <hamm.daniel@gmail.com>
2023-12-23 06:42:00 +0100

    Added comments to the trained models.

--- src/play/trained_model.py
+++ src/play/trained_model.py
@@ -34,27 +34,67 @@ class TrainedModel:
 
 
 device = get_device()
+# trained on 8.6 million positions (no mate positions)
+# batch size 1024
 model_wild_snow_28 = TrainedModel("wild-snow-28.pth", ChessmaitMlp1(), 'fen_to_tensor_one_board', device,
                                   use_normalization=True, max_evaluation=20000, min_evaluation=-20000)
+
+# trained on 4.8 million positions (no mate positions)
+# batch size 2048
 model_fluent_mountain_47 = TrainedModel("fluent-mountain-47.pth", ChessmaitMlp5(), 'fen_to_tensor_one_board', device,
                                         use_normalization=True, max_evaluation=14105, min_evaluation=-14905)
+
+# trained on 3.7 million positions (no mate positions)
+# batch size 256
 model_divine_leaf_29 = TrainedModel("divine-leaf-29.pth", ChessmaitMlp4(), 'fen_to_tensor_one_board', device,
                                     use_normalization=True, max_evaluation=12352, min_evaluation=-12349)
+
+# trained on 1.7 million positions (no mate positions)
+# batch size 1024
 model_honest_dragon_72 = TrainedModel("honest-dragon-72.pth", ChessmaitCnn2NonHot(), 'fen_to_cnn_tensor_non_hot_enc',
                                       device, use_normalization=True, max_evaluation=15265, min_evaluation=-15265)
+
+# trained on 1.7 million positions (no mate positions)
+# batch size 1024
 model_upbeat_cloud_79 = TrainedModel("upbeat-cloud-79.pth", ChessmaitMlp5(), 'fen_to_tensor_one_board', device)
 
+####################
 # models for contest
+####################
+
+# trained on 20.000 games
+# batch size 512
 model_smart_valley_6 = TrainedModel("smart-valley-6.pth", ChessmaitMlp1(), 'fen_to_tensor_one_board', device,
                                     use_normalization=True, max_evaluation=20000, min_evaluation=-20000)
+
+# trained on 4.1 million positions (no mate positions)
+# batch size 1024
 model_firm_star_24 = TrainedModel("firm-star-24.pth", ChessmaitMlp1(), 'fen_to_tensor_one_board', device,
                                   use_normalization=True, max_evaluation=12352, min_evaluation=-12349)
+
+# trained on 5.2 million positions (no mate positions)
+# batch size 1024
 model_lemon_plasma_103 = TrainedModel("lemon-plasma-103.pth", ChessmaitMlp5(), 'fen_to_tensor_one_board', device)
+
+# trained on 7.0 million positions (no mate positions)
+# batch size 1024
 model_stellar_sound_105 = TrainedModel("stellar-sound-105.pth", ChessmaitMlp5(), 'fen_to_tensor_one_board', device)
+
+# trained on 5.2 million positions (no mate positions)
+# batch size 256
 model_effortless_vortex_142 = TrainedModel("effortless-vortex-142.pth", ChessmaitCnn4Bitboard(), 'fen_to_bitboard',
                                            device)
+
+# trained on 15.8 million positions (no mate positions)
+# batch size 1024
 model_graceful_glitter_166 = TrainedModel("graceful-glitter-166.pth", ChessmaitMlp5(), 'fen_to_tensor_one_board',
                                           device)
+
+# trained on 17.3 million positions, including mate positions
+# batch size 1024
 model_apricot_armadillo_167 = TrainedModel("apricot-armadillo-167.pth", ChessmaitMlp5(), 'fen_to_tensor_one_board',
                                            device)
+
+# trained on 7.7 million positions, including mate positoins
+# batch size 256
 model_fresh_blaze_174 = TrainedModel("fresh-blaze-174.pth", ChessmaitCnn4Bitboard(), 'fen_to_bitboard', device)
